04955dee6635a2bf350b1ef7150ce739ffddc71d
gabrielprogit <gflimao4717@gmail.com>
2017-09-29 14:50:38 -0300

    Exercicios web novos

--- src/main/java1/br/com/db1/controller/ExerciciosController.java
+++ src/main/java1/br/com/db1/controller/ExerciciosController.java
@@ -8,36 +8,106 @@ public class ExerciciosController {
 	private Double resultado;
 	private Double valor1;
 	private Double valor2;
-	public Double getResultado() {
-		return resultado;
-	}
-
+	private String texto;
+	private String novoTexto;
+	private Double numeroFormatado;
+	private Boolean java;
+	private Boolean php;
+	private Boolean python;
+	private Boolean cMaisMais;
+	private Double numero;
+	private Uf uf = Uf.PR;
+	private Sexo sexo;
 	
-	public Double exibirResultado() {
-		return resultado;
-	}
-	public void setResultado(Double resultado) {
-		this.resultado = resultado;
-	}
-
-	public void somar() {
-		resultado = valor1 + valor2;
-		
-	}
-
-	public void subtrair() {
-		resultado = valor1 - valor2;
 	
+	public Sexo[] getListaSexo() {
+		return sexo.values();
+	}
+	
+	public Sexo getSexo() {
+		return sexo;
 	}
 
-	public void dividir() {
-		resultado = valor1/valor2;
-		
+
+	public Uf[] getListaUf() {
+		return uf.values();
+	}
+	
+	
+	public Uf getUf() {
+		return uf;
 	}
 
-	public void multiplicar() {
-		resultado = valor1 * valor2;
-		
+	public Double getNumeroFormatado() {
+		return numeroFormatado;
+	}
+
+	public void setNumeroFormatado(Double numeroFormatado) {
+		this.numeroFormatado = numeroFormatado;
+	}
+	
+	public Double getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Double numero) {
+		this.numero = numero;
+	}
+
+	public Boolean getJava() {
+		return java;
+	}
+
+	public void setJava(Boolean java) {
+		this.java = java;
+	}
+
+	public Boolean getPhp() {
+		return php;
+	}
+
+	public void setPhp(Boolean php) {
+		this.php = php;
+	}
+
+	public Boolean getPython() {
+		return python;
+	}
+
+	public void setPython(Boolean python) {
+		this.python = python;
+	}
+
+	public Boolean getcMaisMais() {
+		return cMaisMais;
+	}
+
+	public void setcMaisMais(Boolean cMaisMais) {
+		this.cMaisMais = cMaisMais;
+	}
+
+	public String getNovoTexto() {
+		return novoTexto;
+	}
+
+	public void setNovoTexto(String novoTexto) {
+		this.novoTexto = novoTexto;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Double getValor1() {
+		return valor1;
+	}
+
+	public void setValor1(Double valor1) {
+		this.valor1 = valor1;
 	}
 
 	public Double getValor2() {
@@ -48,11 +118,44 @@ public class ExerciciosController {
 		this.valor2 = valor2;
 	}
 
-	public Double getValor1() {
-		return valor1;
+	public Double getResultado() {
+		return resultado;
 	}
 
-	public void setValor1(Double valor1) {
-		this.valor1 = valor1;
+	public void setResultado(Double resultado) {
+		this.resultado = resultado;
 	}
+	
+	
+
+	public void somar() {
+		setResultado(valor1 + valor2);
+	}
+	
+	public void subtrair() {
+		setResultado(valor1 - valor2);
+	}
+	
+	public void multiplicar() {
+		setResultado(valor1 * valor2);
+	}
+	
+	public void dividir() {
+		setResultado(valor1 / valor2);	
+	}
+	
+	public void maiuscula() {
+		novoTexto = texto.toUpperCase();
+	}
+	
+	public void minuscula() {
+		novoTexto = texto.toLowerCase();
+	}
+	
+	public void formatar() {
+		numeroFormatado = numero;
+	}
+
+	
+	
 }
